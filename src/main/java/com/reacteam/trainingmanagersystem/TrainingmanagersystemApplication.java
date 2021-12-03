4e287a9de2dbe125c135331adf42f1c693413af8
EdgarAnd123 <sancheze82@uabc.edu.mx>
2021-12-02 22:00:13 -0800

    Spring Project

---
+++ src/main/java/com/reacteam/trainingmanagersystem/TrainingmanagersystemApplication.java
@@ -0,0 +1,13 @@
+package com.reacteam.trainingmanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingmanagersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingmanagersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reacteam/trainingmanagersystem/TrainingmanagersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reacteam.trainingmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingmanagersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
